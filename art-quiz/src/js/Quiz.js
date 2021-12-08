fd33734c57c1b8c1079961e23f865a2714931d2f
alexpataman[mac] <alex.pataman@gmail.com>
2021-12-08 13:05:29 +0100

    refactor: ged rid of redundant async declarations in Quiz.js

--- art-quiz/src/js/Quiz.js
+++ art-quiz/src/js/Quiz.js
@@ -100,7 +100,7 @@ export default class Quiz {
     this.app.layout.addBackLink(this.showHomePage, this);
   }
 
-  async startQuestion(roundIndex, questionIndex = 0) {
+  startQuestion(roundIndex, questionIndex = 0) {
     this.variables.currentQuestionId = questionIndex;
     this.variables.currentQuestion = this.getQuestion(roundIndex, questionIndex);
     this.variables.currentAnswerOptions = Quiz.shuffleOptions(
@@ -108,7 +108,7 @@ export default class Quiz {
       this.getWrongOptions(),
     );
 
-    await this.app.layout.setPageContent(this.getQuestionPageContent(), 'question');
+    this.app.layout.setPageContent(this.getQuestionPageContent(), 'question');
     this.app.layout.addBackLink(this.showRoundSelectorPage, this);
     this.preloadNextQuestionImages();
     this.variables.timer.init();
